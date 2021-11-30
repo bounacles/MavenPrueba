22677e379652ba2f4253a07c7fedfcaedbfc31fd
Pablo Rubio Amondarain <pablorubioamodnarain@gmail.com>
2021-11-30 11:02:38 +0100

    Creado LifeAction y test

---
+++ src/main/java/com/mycompany/ghamaven/App/LifeActions.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ghamaven.App;
+
+import com.mycompany.ghamaven.Persona;
+import java.lang.*;
+import org.apache.commons.lang.NotImplementedException;
+
+/**
+ *
+ * @author pablo
+ */
+public class LifeActions {
+    
+    static void morir (Persona persona)
+    {
+        //
+    }
+    
+    static boolean mayorDeEdad(Persona persona)
+    {
+        throw new NotImplementedException("no implementado");
+    }
+    
+}
---
+++ src/test/java/com/mycompany/ghamaven/App/LifeActionsIT.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ghamaven.App;
+
+import com.mycompany.ghamaven.Persona;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pablo
+ */
+public class LifeActionsIT {
+    
+    public LifeActionsIT() {
+    }
+
+    @Test
+    public void testDead() {
+        
+        Persona persona = new Persona("a","b",80);
+        boolean resultA = persona.isAlive();
+        boolean expResultA = true;
+        
+        LifeActions.morir(persona);
+        
+        boolean resultD = persona.isAlive();
+        boolean expResultD = true;
+        
+        assertEquals(expResultA, resultA);
+        assertEquals(expResultD, resultD);
+        
+    }
+    
+    @Test
+    public void testMayorDeEdad() {
+        
+        Persona persona17 = new Persona("a","b",17);
+        Persona persona18 = new Persona("a","b",18);
+        Persona persona19 = new Persona("a","b",19);
+        
+        boolean result17 = LifeActions.mayorDeEdad(persona17);
+        boolean result18 = LifeActions.mayorDeEdad(persona18);
+        boolean result19 = LifeActions.mayorDeEdad(persona19);
+        
+        boolean expResult17 = false;
+        boolean expResult18 = true;
+        boolean expResult19 = true;
+        
+        assertEquals(expResult17, result17);
+        assertEquals(expResult18, result18);
+        assertEquals(expResult19, result19);
+        
+    }
+}
